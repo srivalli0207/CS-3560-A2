2f5eebaa6276cc34bbea9dc43a501e150279135b
Srivalli Kakumani <144284239+srivalli0207@users.noreply.github.com>
2024-06-29 12:14:18 -0700

    Update User.java

--- User.java
+++ User.java
@@ -3,8 +3,7 @@ package a2;
 import java.util.ArrayList;
 import java.util.List;
 
-public class User 
-{
+public class User {
 
     private String userId;
     private List<String> followers;
@@ -12,63 +11,53 @@ public class User
     private List<String> newsFeed;
     private int tweetCount;
 
-    public User(String userId) 
-    {
+    public User(String userId) {
         this.userId = userId;
-        followers = new ArrayList<>();
-        following = new ArrayList<>();
-        newsFeed = new ArrayList<>();
-        tweetCount = 0;
+        this.followers = new ArrayList<>();
+        this.following = new ArrayList<>();
+        this.newsFeed = new ArrayList<>();
+        this.tweetCount = 0; // Initialize tweet count to zero
     }
 
-    public String getUserId() 
-    {
+    public String getUserId() {
         return userId;
     }
 
-    public void postTweet(String message, TwitterService twitterService) 
-    {
+    public void postTweet(String message, TwitterService twitterService) {
         newsFeed.add(message);
         tweetCount++; // Increment tweet count
         // Update followers' news feeds
         twitterService.updateFollowersNewsFeed(userId, message);
     }
-    
-    public int getTweetCount() 
-    {
+
+    public int getTweetCount() {
         return tweetCount;
     }
 
-    public void addFollower(String followerId) 
-    {
+    public void addFollower(String followerId) {
         followers.add(followerId);
     }
 
-    public void addFollowing(String followingId) 
-    {
+    public void addFollowing(String followingId) {
         following.add(followingId);
     }
 
-    public List<String> getFollowers()
-    {
+    public List<String> getFollowers() {
         return followers;
     }
 
-    public List<String> getFollowing() 
-    {
+    public List<String> getFollowing() {
         return following;
     }
 
-    public List<String> getNewsFeed() 
-    {
+    public List<String> getNewsFeed() {
         return newsFeed;
     }
 
-    public void addToNewsFeed(String message) 
-    {
+    public void addToNewsFeed(String message) {
         newsFeed.add(message);
     }
-    
+
     public int getDistinctTweetCount() {
         return newsFeed.size(); // Assuming newsFeed contains distinct tweets
     }
